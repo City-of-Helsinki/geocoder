6c1d10f6f46dbfb9b656965842690496a26df010
Juha Yrjölä <juha.yrjola@iki.fi>
2013-03-30 01:31:05 +0200

    Add support for URL prefix

--- geocoder/urls.py
+++ geocoder/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from tastypie.api import Api
 from geo.api import *
 from geo.views import *
@@ -16,7 +17,7 @@ v1_api.register(AddressResource())
 v1_api.register(POICategoryResource())
 v1_api.register(POIResource())
 
-urlpatterns = patterns('',
+base_urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'geocoder.views.home', name='home'),
     # url(r'^geocoder/', include('geocoder.foo.urls')),
@@ -26,8 +27,13 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-    url(r"^api/", include(v1_api.urls)),
+    url(r"^", include(v1_api.urls)),
     url(r"^demo/$", DemoView.as_view()),
     url(r"^google/autocomplete/$", google_autocomplete),
     url(r"^google/details/$", google_details),
+)
+
+prefix = getattr(settings, 'URL_PREFIX', '')
+urlpatterns = patterns('',
+    url("^" + prefix, include(base_urlpatterns))
 )
